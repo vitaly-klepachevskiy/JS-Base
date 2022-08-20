f174c3bc7a63042c349aaa8aba52ac572fd896dd
Vitaly Klepachevskiy <vitaly.klepachevskiy@gmail.com>
2022-08-20 23:40:39 +0300

    Web: comment is added

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var buttonPause = document.getElementById('button-pause');
 
 var Interval ;
 var images = [
-    '1', '2', '3', '4', '5', '6', '7', '8',  '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];
+    '1', '2', '3', '4', '5', '6', '7', '8',  '9', '10', '11', '12', '13', '14', '15', '16', '17', '18'];  //delete hardcode
 var clone = images.slice(0); // duplicate array
 var cards = images.concat(clone); // merge to arrays
 
